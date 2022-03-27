b882c1eaca5a2c9529daf8a74f9109efd0f75cf2
Ramil Rakhamimov <rakhamimovramil@gmail.com>
2022-03-27 16:15:05 -0400

    Server side done, client side utils done

---
+++ client/src/utils/queries.js
@@ -0,0 +1,20 @@
+import { gql } from "@apollo/client";
+
+
+export const QUERY_ME = gql`
+query Query {
+    me {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`
